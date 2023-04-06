fb087c1a842cf85146c2bd943d66f8c372b396f8
Roberto Carlos Cruz Rodriguez <roberto.cruz.rdg@gmail.com>
2023-04-06 16:15:02 -0400

    add deploy script

---
+++ deploy_app.sh
@@ -0,0 +1,15 @@
+QT_VERSION=6.5.0
+export QT_QMAKE_EXECUTABLE=/home/programs/Qt/${QT_VERSION}/gcc_64/bin/qmake
+export QT_DIR=/home/programs/Qt/${QT_VERSION}/gcc_64/lib/cmake/Qt6
+export CMAKE_PREFIX_PATH=/home/programs/Qt/${QT_VERSION}/gcc_64
+export QT_QMAKE_EXECUTABLE=/home/programs/Qt/${QT_VERSION}/gcc_64/bin/qmake
+export PATH=/home/programs/Qt/${QT_VERSION}/gcc_64/bin/:$PATH
+
+CURR_DIR=$PWD 
+
+mkdir -p deploy && cd deploy 
+qt-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../app_bin ../src
+make -j6 && make install 
+
+# deb packing 
+cpack -G DEB
